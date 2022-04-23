5e05e74914403fe01a93478fc04293886b8c2339
khaopoon2543 <khaopoon2543@gmail.com>
2022-04-23 17:15:13 +0700

    big update !!!

--- routes/spotify.js
+++ routes/spotify.js
@@ -6,15 +6,36 @@ scopes = ['user-read-private','user-read-currently-playing']
 
 /* ----------------------------------------------------------------------------------------------------------- */
 
+app.post('/refresh', async (req,res) => {
+  console.log('hi')
+  const refreshToken = req.body.refreshToken
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URL,
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    refreshToken,
+  });
+
+  spotifyApi.refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken : data.body.access_token,
+        expiresIn : data.body.expires_in
+      })
+    })
+    .catch(() => {
+      res.sendStatus(400)
+    })
+});
+
 app.post('/login', async (req,res) => {
+  const code = req.body.code;
   const spotifyApi = new SpotifyWebApi({
     redirectUri: process.env.REDIRECT_URL,
     clientId: process.env.SPOTIFY_CLIENT_ID,
     clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
   });
   
-  const code = req.body.code;
-  console.log(code)
   spotifyApi.authorizationCodeGrant(code)
     .then(data => {
       console.log(data.body.access_token)
